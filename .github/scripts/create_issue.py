9d3155e2689a2b04435d3c28b64eb32e5c1ec4f9
jz <t.jingzhi1@gmail.com>
2023-10-10 15:49:25 +0800

    do not create duplicate issues

--- .github/scripts/create_issue.py
+++ .github/scripts/create_issue.py
@@ -11,10 +11,19 @@ repo = g.get_repo(REPO_NAME)
 with open('results.json', 'r') as f:
     results = json.load(f)
 
+def issue_exists(title):
+    open_issues = repo.get_issues(state='open')
+    for issue in open_issues:
+        if issue.title == title:
+            return True
+    return False
 
 for finding in results['results']:
     if 'severity' in finding['extra'] and finding['extra']['is_ignored'] == False:
+        
         title = f"Semgrep Error: {finding['check_id']}"
+        if issue_exists(title):
+            continue
         body = f"""
 Severity: {finding['extra']['severity']}
 Path: {finding['path']}
