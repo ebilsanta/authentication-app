662ad9bc72c220d0bc4a2a5126f77c3ae6d23d51
elvisyong <jolene.yeo.2021@scis.smu.edu.sg>
2023-11-11 06:14:45 +0800

    add dockerfile and todo

--- resource/src/main.py
+++ resource/src/main.py
@@ -1,9 +1,9 @@
-from typing import Union
 from fastapi import FastAPI
 from database.users import get_user
+
 app = FastAPI()
 
-# TODO: Find out whats the endpoint gonna be like 
+# TODO: Verify the token from the request
 @app.get("/users/{company}/{email}")
 def get_user_handler(company: str, email: str):
     user = get_user(company, email)
