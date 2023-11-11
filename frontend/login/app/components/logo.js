69d29f5e772d2989214b732c45807d50fd918e7e
Jing Zhi <t.jingzhi1@gmail.com>
2023-11-11 23:31:42 +0800

    [skip ci] back to / when logo is pressed

--- frontend/login/app/components/logo.js
+++ frontend/login/app/components/logo.js
@@ -1,12 +1,18 @@
+'use client'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 export default function Logo() {
+  const router = useRouter()
   return (
     <Image
-      className='pt-10 pl-10 mb-10'
+      className='pt-10 pl-10 mb-10 cursor-pointer'
       src="/logo.svg"
       alt="Ascenda Loyalty"
       width={150}
       height={30}
       priority
+      onClick={() => {
+        router.push('/')
+      }}
     />)
 }
