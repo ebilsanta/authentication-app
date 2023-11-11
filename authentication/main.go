0294ca1a341879866770a1bb82eca4ffaf7627ed
Wei Bin <auyeongweibin@gmail.com>
2023-11-11 23:00:43 +0800

    log errors

--- authentication/main.go
+++ authentication/main.go
@@ -192,12 +192,10 @@ func ResponseMessage(input map[string]string, callback string, message sqs.Messa
 	_, err := url.ParseRequestURI(callback)
 	if err != nil {
 		log.Println("Error with callback URL: ", err)
-		return nil
 	}
 	resp, err := http.Post(callback, "application/json", responseBody)
 	if err != nil {
 		log.Println("An Error Occured: ", err)
-		return nil
 	}
 	defer resp.Body.Close()
 	// body, err := ioutil.ReadAll(resp.Body)
@@ -214,7 +212,6 @@ func ResponseMessage(input map[string]string, callback string, message sqs.Messa
 	_, err = queue.DeleteMessage(deleteParams)
 	if err != nil {
 		log.Println(err)
-		return nil
 	}
 	return nil
 }
