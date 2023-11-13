1f4fdfb2e9c8b45f5f0db9a1cb63128414589463
Jing Zhi <t.jingzhi1@gmail.com>
2023-11-14 04:36:27 +0800

    updated k6 script

--- k6/script.js
+++ k6/script.js
@@ -27,7 +27,7 @@ export let options = {
             startVUs: 100,
             stages: [
                 { duration: '2m', target: 500 }, // Ramp up
-                { duration: '3m', target: 1000}, // Stay at peak
+                { duration: '3m', target: 5000}, // Stay at peak
                 { duration: '2m', target: 0 },  // Ramp down
             ],
             startTime: '8m',
@@ -50,24 +50,24 @@ export let options = {
             exec: 'testFunction',
             startVUs: 1,
             stages: [
-                { duration: '10m', target: 50000 }, // Gradually ramp up
+                { duration: '10m', target: 20000 }, // Gradually ramp up
             ],
             startTime: '18m',
             tags: { test_type: 'breakpoint' },
         },
-        soakTest: {
-            executor: 'constant-vus',
-            exec: 'testFunction',
-            vus: 100,
-            duration: '60m', // Long duration
-            startTime: '29m',
-            tags: { test_type: 'soak' },
-        }
+        //soakTest: {
+        //    executor: 'constant-vus',
+        //    exec: 'testFunction',
+        //    vus: 100,
+        //   duration: '60m', // Long duration
+        //    startTime: '29m',
+        //    tags: { test_type: 'soak' },
+        //}
     },
 };
 
 export function testFunction () {
-    const url = 'https://client.itsag2t1.com/api/login';
+    const url = 'https://client.itsag2t1.com/api/hosted/login';
     const payload = JSON.stringify({
         email: 'auyeongweibin@gmail.com',
         password: 'password',
