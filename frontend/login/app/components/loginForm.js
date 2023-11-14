4e7dd61fddd59c3ca68934c8fb9c9a864dcd1b8e
Jing Zhi <t.jingzhi1@gmail.com>
2023-11-14 18:51:41 +0800

    added loginform check, fixed form inputs have no value

--- frontend/login/app/components/loginForm.js
+++ frontend/login/app/components/loginForm.js
@@ -7,7 +7,7 @@ export default function LoginForm() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const { hostedLogin, setHostedLoginContext } = useLogin();
-
+  const [errMsg, setErrMsg] = useState("");
 
   const router = useRouter()
 
@@ -33,25 +33,30 @@ export default function LoginForm() {
     const data = await res.json()
     console.log(data)
     if (res.status === 200) {
+      setErrMsg("")
       setHostedLoginContext(true)
       console.log(hostedLogin)
       router.push("/home")
     } else {
+      setErrMsg("Invalid email or password!")
+      setEmail("")
+      setPassword("")
       return
     }
   }
 
   return <div className="bg-white flex flex-col lg:w-1/3 w-4/5 -mt-20 mx-auto rounded-md shadow">
     <h1 className="text-black font-semibold p-5 text-center text-xl">Log in to your account</h1>
+    {errMsg !== "" && <p className="text-red-500 text-center">{errMsg}</p>}
     <form onSubmit={(e) => {onSubmit(e)}} className="px-8 pb-8 mb-4">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
         Email
       </label>
-      <input onChange={(e) => setEmail(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Enter your email" />
+      <input value={email} onChange={(e) => setEmail(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Enter your email" />
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
         Password
       </label>
-      <input onChange={(e) => setPassword(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="Enter your password"/>
+      <input value={password} onChange={(e) => setPassword(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="Enter your password"/>
       <button className="bg-[#1e244d] shadow hover:bg-blue-700 mt-3 w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300" type="submit">
         Sign In
       </button>
