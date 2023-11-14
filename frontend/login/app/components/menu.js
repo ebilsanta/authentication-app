7ab4c9c9adecd5afe617bd938619337b8dd42cdf
Jing Zhi <t.jingzhi1@gmail.com>
2023-11-14 23:37:57 +0800

    editing / page for consistency during presntation

--- frontend/login/app/components/menu.js
+++ frontend/login/app/components/menu.js
@@ -1,12 +1,22 @@
 import Link from 'next/link';
 
 export default function Menu() {
-    return <div className="bg-white rounded-md p-5 m-auto">
+    return <div className="bg-white rounded-md p-5 m-auto w-1/3">
       <div>
-        <button className="peer text-[#1e244d] font-semibold py-5 text-4xl">Let's get started</button>
-        <div class="hidden peer-hover:flex hover:flex w-[300px] flex-col drop-shadow-lg">
-          <Link href="/register" className="hover:text-white text-gray-500 font-semibold py-5 text-xl hover:duration-500">Register</Link>
-          <Link href="/login" className="hover:text-white text-gray-500 font-semibold py-5 text-xl hover:duration-500">Login</Link>
+        <button className="text-black font-semibold py-5 text-xl">Let's get started</button>
+        <div>
+        <Link href="/login"> 
+          <button className="my-5 transition duration-300 bg-[#1e244d] shadow hover:bg-blue-700 w-full text-white font-semibold py-2 px-5 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Login
+          </button>
+          </Link>
+
+          <hr></hr>
+         <Link href="/register"> 
+          <button className="my-5 transition duration-300 bg-[#1e244d] shadow hover:bg-blue-700 w-full text-white font-semibold py-2 px-5 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Register
+          </button>
+          </Link>
         </div>
       </div>
     </div>
