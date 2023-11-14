b142842ba6bbad852739853cd5fb03e5d42b829b
Jing Zhi <t.jingzhi1@gmail.com>
2023-11-14 22:49:27 +0800

    removed check for /user

--- frontend/login/app/home/page.js
+++ frontend/login/app/home/page.js
@@ -6,26 +6,26 @@ import Home from '../components/home'
 
 export default function HomePage(props) { 
   const router = useRouter()
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://client.itsag2t1.com/api/" + "hosted/user"
-      , {
-        method: "GET",
-        credentials: "include",
-        headers : {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-      })
-      const data = await res.json()
-      if (res.status != 200) {
-        router.push("/login")
-      }
-    }
-    fetchData().catch((err) => {
-      router.push("/login")
-    })
-}, [])
+  //useEffect(() => {
+  //  const fetchData = async () => {
+  //   const res = await fetch("https://client.itsag2t1.com/api/" + "hosted/user"
+  //    , {
+  //      method: "GET",
+  //      credentials: "include",
+  //      headers : {
+  //          'Accept': 'application/json',
+  //          'Content-Type': 'application/json'
+  //      }
+   //   })
+    //  const data = await res.json()
+  //    if (res.status != 200) {
+  //      router.push("/login")
+  //    }
+  //  }
+  //  fetchData().catch((err) => {
+  //    router.push("/login")
+  //  })
+//}, [])
 
 
   return <Home />
